c56c066812e0ea50413f9cbc4ba5004c14ff0847
Collino <coadhola@gmail.com>
2022-01-09 03:37:23 +0000

    Login feature

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+//////// BANKIST APP ///////////////////////////
 
 // Data
 const account1 = {
@@ -72,13 +70,12 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov} €</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -91,12 +88,49 @@ const createUsernames = function (accs) {
     return acc.username;
   });
 };
-console.log(createUsernames(accounts));
-console.log(accounts);
+createUsernames(accounts);
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//Event handler
+let currentAccount; // out of func bcoz needed in other functions
+
+btnLogin.addEventListener('click', function (e) {
+  //Prevent form from submitting
+  e.preventDefault();
+  //find account and owner username should match the value entered
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  //? allows for pin to be read only if currentAccount exists. returns undefined
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //input always string and needs to be turned to number
+    
+    //Display UI & Welcome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    //Display summary
+
+    calcDisplaySummary(currentAccount);
+
+    //Display movements
+    displayMovements(currentAccount.movements);
+
+    //Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    console.log('Loginpin');
+  }
+});
+
+/////////////////////+LECTURES+////////////////////////////
+
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -111,9 +145,9 @@ const arr = ['a', 'b', 'c', 'd', 'e'];
 
 movements.forEach(function (move, i) {
   if (move > 0) {
-    console.log(`you have made ${move} deposits`);
+    //console.log(`you have made ${move} deposits`);
   } else {
-    console.log(`you have made  ${Math.abs(move)} Withdrawals`);
+    //console.log(`you have made  ${Math.abs(move)} Withdrawals`);
   }
 });
 
@@ -124,13 +158,13 @@ const deposits = movements.filter(function (mov) {
 
 //Use for in filters
 const depositFor = [];
-for (const mov of movements) if (mov > 0) depositFor.push(mov);
-console.log(depositFor);
+  for (const mov of movements) if (mov > 0) depositFor.push(mov);
+// console.log(depositFor);
 
 const withdrawals = [];
-for (const mov of movements) if (mov < 0) withdrawals.push(mov);
+  for (const mov of movements) if (mov < 0) withdrawals.push(mov);
 
-console.log(withdrawals);
+// console.log(withdrawals);
 
 //*************reduce* Adds array elements from accumilator initial***** */
 // const balance = movements.reduce(function(acc, curr, i, arr){
@@ -141,46 +175,54 @@ console.log(withdrawals);
 
 //Simple arrow from above
 const balance2 = movements.reduce((acc, curr) => acc + curr, 0);
-console.log(balance2);
+// console.log(balance2);
 
 // for loops in filter
 let newReduce = 0;
-for (const mov of movements) newReduce += mov;
+  for (const mov of movements) newReduce += mov;
 
-console.log(newReduce);
+// console.log(newReduce);
 
 //++++++Display balance on app+++++++++
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, curr) => acc + curr, 0);
-  labelBalance.textContent = `${balance} GBP`;
+  labelBalance.textContent = `${balance}€`;
+  labelBalance.textContent = `${balance}`;
 };
-
-calcDisplayBalance(account1.movements);
 
 // Max value with reduce
 const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
-}, movements[0]);
-console.log(max);
+  }, movements[0]);
+// console.log(max);
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, curr) => acc + curr);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter(int => int >= 1) //includng only interests greater tha 1
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
 
 
 //Chaining....pipeline
 const euroToUSD = 1.1;
-
 const totalDepositsUSD = movements
-.filter(mov => mov > 0)
-.map(mov=> mov * euroToUSD)
-.reduce((acc,mov) => acc + mov,0);
+  .filter(mov => mov > 0)
+  .map(mov => mov * euroToUSD)
+  .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
-labelSumIn.textContent = totalDepositsUSD.toFixed(2);
-
-const totalWithdrawalsUSD = movements
-.filter(mov => mov < 0)
-.map((mov=> mov * euroToUSD))
-.reduce((acc, curr)=> Math.abs(acc + curr))
-// .parseFloat(totalWithdrawalsUSD).fixed(2);
-console.log(totalWithdrawalsUSD.toFixed(2));
-//Math.round(totalWithdrawalsUSD);
-labelSumOut.textContent = totalWithdrawalsUSD.toFixed(2);
+//labelSumIn.textContent = totalDepositsUSD.toFixed(2);
